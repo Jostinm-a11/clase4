a9fee033f5c38c7050900d46ce9efd61d2efda43
Jostinm-a11 <nana1406.m@gmail.com>
2024-10-03 22:37:55 -0400

    Add files via upload

---
+++ tarea 4/Untitled7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(void)
+{
+int I, K = 4;
+for (I = 1; I <= 3; I++)
+{
+    printf(“\n\nValor de K antes de llamar a la función: %d”, ++K);
+printf(“\nValor de K después de llamar a la función: %d”, f1(K));
+}
+}
+int f1(int R)
+{
+R += R;
+return (R);
+}
